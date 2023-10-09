95e8a300924eaaacd80e8b52a8fc57f8a9e7ed7d
Andre Abad <andreabad17@gmail.com>
2023-10-08 20:51:14 -0700

    Working on the calender

---
+++ circle_site/src/components/Calendar/Calendar.jsx
@@ -0,0 +1,9 @@
+function Calendar() {
+    return (
+        <>
+            <iframe src="https://embed.styledcalendar.com/#QKPYSeHdKaqs3xz1Ni1x" title="Styled Calendar" className="styled-calendar-container" style={{ width: "100%", height: "40rem" }} data-cy="calendar-embed-iframe"></iframe>
+        </>
+    )
+}
+
+export default Calendar
--- circle_site/src/components/HomePage/HomePage.jsx
+++ circle_site/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
+import Calendar from "../Calendar/Calendar"
+
 function HomePage() {
     return (
         <>
             <h1>This is HomePage</h1>
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=YW5kcmVhYmFkMTdAZ21haWwuY29t&src=YnJpYW5mb25nMjJAZ21haWwuY29t&src=ZnJpYW5ib25nQGdtYWlsLmNvbQ&src=c2FuZHJhLmxpbi4wMjA2QGdtYWlsLmNvbQ&color=%237bb241&color=%23795548&color=%23E4C441&color=%23D81B60" style={{border:"solid 1px #777"}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>         </>
+            <Calendar/>
+        </>
+
     )
 }
 
