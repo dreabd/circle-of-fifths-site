3e5a499909e675d815f394c7939909e0df120066
Andre Abad <andreabad17@gmail.com>
2023-10-09 21:25:24 -0700

    Workign on the homepage

--- circle_site/src/components/HomePage/HomePage.jsx
+++ circle_site/src/components/HomePage/HomePage.jsx
@@ -4,8 +4,7 @@ function HomePage() {
     return (
         <div className="homepage">
             <h1>This is HomePage</h1>
-            {/* <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=YW5kcmVhYmFkMTdAZ21haWwuY29t&src=YnJpYW5mb25nMjJAZ21haWwuY29t&src=ZnJpYW5ib25nQGdtYWlsLmNvbQ&src=c2FuZHJhLmxpbi4wMjA2QGdtYWlsLmNvbQ&color=%237bb241&color=%23795548&color=%23E4C441&color=%23D81B60" style={{ border: "solid 1px #777" }} width="800" height="600" frameBorder="0" scrolling="no"></iframe> */}
-        </div>
+            <iframe src="https://embed.styledcalendar.com/#QKPYSeHdKaqs3xz1Ni1x" title="Styled Calendar" className="styled-calendar-container" data-cy="calendar-embed-iframe"></iframe>        </div>
     )
 }
 
--- circle_site/src/components/Navbar/Navbar.jsx
+++ circle_site/src/components/Navbar/Navbar.jsx
@@ -13,25 +13,23 @@ function Navbar({ user }) {
             <button className={`close-button ${display ? "open-slide" : "close-slide"}`} onClick={toggleNav}>
                 <i style={{ color: "white" }} className={`${display ? "fa-solid fa-chevron-left" : "fa-solid fa-chevron-right"}`}></i>
             </button>
-            {display &&
-                <div className="user-container">
-                    <div className="user-profile">
-                        <img src={user.pfp} alt="userPfp" />
-                        <p>Welcome, {user.firstName}</p>
-                    </div>
-                    <div className="navlinks">
-                        <NavLink exact to="/">Home</NavLink>
-                        <NavLink exact to="/announcements">Announcements</NavLink>
-                        <NavLink exact to="/calendar">Calendar</NavLink>
-                        <NavLink exact to="/music">Music</NavLink>
-                        <NavLink exact to="/media">Media</NavLink>
-                        <NavLink exact to="/discussions">Discussions</NavLink>
-                        <NavLink exact to="/board">Board</NavLink>
-                        <NavLink exact to="/settings">Settings</NavLink>
-                        <button>Log Out</button>
-                    </div>
+            <div className="user-container">
+                <div className="user-profile">
+                    <img src={user.pfp} alt="userPfp" />
+                    <p>Welcome, {user.firstName}</p>
                 </div>
-            }
+                <div className="navlinks">
+                    <NavLink exact to="/">Home</NavLink>
+                    <NavLink exact to="/announcements">Announcements</NavLink>
+                    <NavLink exact to="/calendar">Calendar</NavLink>
+                    <NavLink exact to="/music">Music</NavLink>
+                    <NavLink exact to="/media">Media</NavLink>
+                    <NavLink exact to="/discussions">Discussions</NavLink>
+                    <NavLink exact to="/board">Board</NavLink>
+                    <NavLink exact to="/settings">Settings</NavLink>
+                    <button>Log Out</button>
+                </div>
+            </div>
         </div>
     )
 }
